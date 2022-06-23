fa0d12909dc9dc3bc6dc33bdae8b9a42207d4046
Kale Leach <kleach1212@gmail.com>
2022-06-23 14:49:30 -0700

    commit tree efb00671e97cf56e01392610c79796989a0b2110

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)  VALUES ('Mr. Chocolate', 10, 'M', 'Brown', 'Nice', 1),
+('Rowdy', 1, 'M', 'Black', 'Mean', 1),
+('Tabitha', 4, 'F', 'White', 'Nice', 1),
+('Sergeant Brown', 6, 'M', 'Brown', 'Nice', 1),
+('Melissa', 12, 'M', 'Brown', 'Nice', 1),
+('Mr. Chocolate', 3, 'M', 'Brown', 'Nice', 1),
+ ('Wendy', 9, 'F', 'Brown', 'Nice', 1),
+ (NULL, 19, 'M', 'Brown', 'Nice', 1)
